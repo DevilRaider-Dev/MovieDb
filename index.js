30c125a76e7a36870bd6db3fc11e77831fceeafe
DevilRaider-Dev <deviltwix3@gmail.com>
2021-10-07 17:25:03 +0200

    add list navigation

--- index.js
+++ index.js
@@ -17,27 +17,30 @@ app.listen(port, () => {
 
 //router
 app.get('/', (reg, res) => {
-    res.render('pages/index.ejs');
+    res.redirect('/movies/1')
+    //res.render('pages/index.ejs');
 });
 
+//backup for uri manipulation
 app.get('/movies', (reg, res) => {
     res.redirect('/movies/1')
 });
 
+//fetch movie list
 app.get('/movies/:page', (reg, res) => {
     axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}&language=de-DE&page=${reg.params.page}`)
         .then(function (response) {
-            res.render('pages/movies.ejs', { movies: response.data.results })
+            res.render('pages/movies.ejs', { movies: response.data, page: Number(reg.params.page) })
         })
         .catch(function (error) {
             console.log(error);
         })
 });
 
+//fetch movie details
 app.get('/details/:id', (reg, res) => {
     axios.get(`https://api.themoviedb.org/3/movie/${reg.params.id}?api_key=${process.env.API_KEY}&language=de-DE`)
         .then(function (response) {
-            console.log(response.data)
             res.render('pages/details.ejs', { details: response.data })
         })
         .catch(function (error) {
